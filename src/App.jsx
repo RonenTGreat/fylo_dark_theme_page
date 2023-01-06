26088470106e550870fb8ae3b9c51e3748701faf
ronentgreat <ronenh53@gmail.com>
2023-01-06 21:26:43 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=' text-3xl text-center text-orange-600'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
