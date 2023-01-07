d732995a0f9dc07ff6d56f95101c5023039e798f
ronentgreat <ronenh53@gmail.com>
2023-01-07 18:02:45 +0000

    done with header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import logo from "../../images/logo.svg"
+
+
+const Header = () => {
+  return (
+    <header className=" font-raleway bg-main-color text-gray-300 flex justify-between p-5">
+      <img className="w-28 object-contain" src={logo} alt="" />
+
+      <nav className="flex gap-6">
+        <a className="hover:text-white hover:underline" href="/#">Features</a>
+        <a className="hover:text-white hover:underline" href="/#">Team</a>
+        <a className="hover:text-white hover:underline" href="/#">Sign In</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
