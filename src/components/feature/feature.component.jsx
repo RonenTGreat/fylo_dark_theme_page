284943b65ce2a24957df9716999aedcb85686504
ronentgreat <ronenh53@gmail.com>
2023-01-08 14:07:59 +0000

    created component for features

---
+++ src/components/feature/feature.component.jsx
@@ -0,0 +1,11 @@
+const Feature = (props) => {
+  return ( 
+    <div className="mt-20">
+      <img className="mx-auto w-16 mb-6" src={props.img} alt="" />
+      <h3 className=" font-raleway text-white text-center font-semibold">{props.head}</h3>
+      <p className=" font-opensans text-gray-300 text-center text-sm mx-auto mt-2 max-w-xs">{props.description}</p>
+    </div>
+   );
+}
+ 
+export default Feature;
