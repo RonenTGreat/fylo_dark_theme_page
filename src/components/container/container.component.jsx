085122e2f52b2faac9b90a09b0551e3c2762c000
ronentgreat <ronenh53@gmail.com>
2023-01-08 13:14:15 +0000

    fixed postion of divs and button

--- src/components/container/container.component.jsx
+++ src/components/container/container.component.jsx
@@ -11,20 +11,25 @@ const Container = () => {
       <section className="bg-intro-color">
         <img className="px-10 pt-4 relative z-30" src={intro} alt="hero image" />
       </section>
-      <section className=" bg-main-color">
+      <section className=" bg-main-color relative">
           <picture>
             <source media="(max-width: 1440px)" srcset={curveDesktop} />
             <img className=" bg-intro-color relative z-10 bottom-7" src={curveMobile} alt="intro" />
           </picture>
 
-          <h1 className="absolute z-10 bottom-24 text-3xl text-white text-center max-w-sm ml-16">All your file in one secure location, accessible anywhere.</h1>
+          <h1 className="font-raleway font-bold relative z-10 bottom-24 text-3xl text-white text-center max-w-sm mx-auto">All your file in one secure location, accessible anywhere.</h1>
 
-          <p className="text-center text-gray-300 max-w-xs mx-auto mt-6">
+          <p className="font-opensans text-ms relative z-10 bottom-20 text-center text-gray-300 max-w-xs mx-auto">
           Fylo stores all your most important files in one secure location. Access them wherever
           you need, share and collaborate with friends family, and co-workers.
           </p>
 
-        <button className=" text-white font-semibold bg-gradient-to-r from-mycyan to-myblue hover:bg-gradient-to-r hover:to-mycyan w-72 ml-24 mt-10 p-3 rounded-full">Get Started</button>
+        <div className="relative z-10 bottom-12 grid place-content-center">
+          <button className=" font-raleway text-white font-semibold bg-gradient-to-r from-mycyan to-myblue hover:bg-gradient-to-r hover:to-mycyan w-72 p-3 rounded-full">Get Started</button>
+        </div>
+      </section>
+      <section>
+        h
       </section>
     </main>
    );
